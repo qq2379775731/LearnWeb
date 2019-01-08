317af2a5e55668f194e8130d1ae22202323ec2ed
hhj <2379775731@qq.com>
2019-01-08 13:50:53 +0800

    the first commit

---
+++ JS Stalker.js
@@ -0,0 +1,5 @@
+var firstname = prompt("what is your first name?");
+var lastname = prompt("what is your last name?");
+var age = prompt("how old are you?");
+console.log(firstname+lastname);
+console.log(age);
